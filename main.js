7e59d3d02f14249ed1ac5b1c86fd39b62da686fe
Jorge Leandro Piva <A517529@totalexpress.com.br>
2022-03-07 20:31:27 -0300

    primeiro comit do projeto

---
+++ main.js
@@ -0,0 +1,21 @@
+const x =2;
+var y = 3;
+let z = 4;
+var faker = require('faker-br');
+
+console.log("Valor de X: "+x);
+console.log("Valor de y: ",y);
+console.log("Valor de z: ",z); 
+
+for (let i = 0; i < 10; i++){
+    console.log("["+i+"] teste");
+} 
+i = "nome"; 
+console.log(i); 
+
+let cpf = faker.cpf();
+
+console.log(cpf); 
+
+//teste
+
