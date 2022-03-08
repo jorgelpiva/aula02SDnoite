d0a3b230bf4769a60b2a9f6f1cece456b880c6c8
Jorge Leandro Piva <A517529@totalexpress.com.br>
2022-03-07 21:08:55 -0300

    removi o fakebr

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const x =2;
 var y = 3;
 let z = 4;
-var faker = require('faker-br');
+
 
 console.log("Valor de X: "+x);
 console.log("Valor de y: ",y);
@@ -11,11 +11,8 @@ for (let i = 0; i < 10; i++){
     console.log("["+i+"] teste");
 } 
 i = "nome"; 
-console.log(i); 
 
-let cpf = faker.cpf();
 
-console.log(cpf); 
 
 //teste
 
